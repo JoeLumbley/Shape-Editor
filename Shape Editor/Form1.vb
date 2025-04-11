0f86ed9e7ae624e0f63e0b0b60ee1ee0b7493487
Joseph Lumbley <joelumbley16@gmail.com>
2025-04-11 11:08:50 -0500

    Fix title bar refresh in Windows 10 dark mode Updated OpenFileDialog filter to default to showing all files, with selectable options for CSV and Text file formats.

--- Shape Editor/Form1.vb
+++ Shape Editor/Form1.vb
@@ -634,7 +634,10 @@ Public Class Form1
             openFileDialog.ShowHelp = False
 
 
-            openFileDialog.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*"
+            'openFileDialog.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*"
+
+            openFileDialog.Filter = "All Files (*.*)|*.*|CSV Files (*.csv)|*.csv|Text Files (*.txt)|*.txt"
+
             openFileDialog.Title = "Open Shape"
             openFileDialog.InitialDirectory = Application.StartupPath
 
