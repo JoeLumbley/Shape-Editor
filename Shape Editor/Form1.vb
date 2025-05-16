80b5fb02c18bd82fa7f57c993dcac20f044497f8
Joseph Lumbley <joelumbley16@gmail.com>
2025-05-15 20:59:11 -0500

    Clean up

--- Shape Editor/Form1.vb
+++ Shape Editor/Form1.vb
@@ -23,7 +23,6 @@
 ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ' SOFTWARE.
 
-Imports System.ComponentModel
 Imports System.IO
 Imports System.Runtime.InteropServices
 
